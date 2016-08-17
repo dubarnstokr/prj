3eaa825e0684871e092015349e176c7e74f330ad
dubarnstokr <maksym.morokko@gmail.com>
2016-08-17 10:47:13 +0100

    commit 2

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.get('/', function (req, res) {
   res.send('Root folder');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+const port = 3000;
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
 });
